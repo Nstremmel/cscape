290f2d9e72d2dd782560d0d6b367a976af5560b5
Noah <noahstremmel22@gmail.com>
2020-04-08 18:49:33 -0500

    fixes

--- cscape.py
+++ cscape.py
@@ -13,12 +13,12 @@ c = conn.cursor()
 conn.set_session(autocommit=True)
 
 #c.execute("DROP TABLE rsmoney")
-c.execute("""CREATE TABLE rsmoney (
-              id bigint,
-              tokens bigint,
-              tokenstotal bigint
-              )""")
-conn.commit()
+# c.execute("""CREATE TABLE rsmoney (
+#               id bigint,
+#               tokens bigint,
+#               tokenstotal bigint
+#               )""")
+# conn.commit()
 
 client = discord.Client()
 
