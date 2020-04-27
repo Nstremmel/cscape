b001a9a871de9c1fc16e134cc3c9b5f20a0dd383
Noah <noahstremmel22@gmail.com>
2020-04-27 17:19:23 -0500

    fix

--- cscape.py
+++ cscape.py
@@ -666,7 +666,7 @@ async def on_message(message):
 
     elif message.content == '!rocktail' or message.content == '!dds' or message.content == '!whip':
         player = getvalue(message.author.id, ['Php', 'Procktails', 'Pspecial', 'Ppoisoned', 'Ppoisonturns', 'Pspecturns'], 'duels')
-        player.insert(0, message.author.id)
+        player.insert(0, message.author)
         bot = getvalue(message.author.id, ['Bhp', 'Brocktails', 'Bspecial', 'Bpoisoned', 'Bpoisonturns', 'Bspecturns'], 'duels')
         bot.insert(0, 'CryptoScape Bot')
         channelid = getvalue(message.author.id, 'channelid', 'duels')
