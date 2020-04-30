8c4a17cbcf52492bf9593ea4ac274dff83b24c0c
Noah <noahstremmel22@gmail.com>
2020-04-29 23:40:41 -0500

    fix

--- cscape.py
+++ cscape.py
@@ -528,6 +528,8 @@ async def on_message(message):
             channels = getvalue(message.author.id, 'channels', 'rsmoney')
             for i in channels.split('|'):
                 if int(i) == message.channel.id:
+                    print(i)
+                    print(str(i))
                     newChannels = (channels.split('|')).remove(str(i))
                     print (channels.split('|'))
                     print(newChannels)
